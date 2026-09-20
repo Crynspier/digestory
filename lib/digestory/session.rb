7be23d251e0c95b5eb7c982c1a4ba26008f0c213
Leon Zjakić <117075643+Crynspier@users.noreply.github.com>
2026-09-20 19:47:55 +0200

    fix: use actual session regex anchor

--- lib/digestory/session.rb
+++ lib/digestory/session.rb
@@ -94,7 +94,7 @@ module Digestory
                          Digest.userhash(
                            username: @username,
                            realm: selected_challenge.realm,
-                           algorithm: selected_challenge.algorithm.sub(/-sess\\z/i, ""),
+                           algorithm: selected_challenge.algorithm.sub(/-sess\z/i, ""),
                            charset: selected_challenge.charset
                          )
                        else
