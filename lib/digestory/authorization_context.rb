6ad88b256bc7ab0ebf5f4a176bf49f3dcda51b7a
Leon Zjakić <117075643+Crynspier@users.noreply.github.com>
2026-09-22 11:56:40 +0200

    feat: add per-request authorization context

---
+++ lib/digestory/authorization_context.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+module Digestory
+  class AuthorizationContext
+    attr_reader :header, :challenge, :request_uri, :method, :qop, :cnonce,
+                :nc, :username, :response, :nonce_key
+
+    def initialize(header:, challenge:, request_uri:, method:, qop:, cnonce:, nc:, username:, response:, nonce_key:)
+      @header = header
+      @challenge = challenge
+      @request_uri = request_uri
+      @method = method
+      @qop = qop
+      @cnonce = cnonce
+      @nc = nc
+      @username = username
+      @response = response
+      @nonce_key = nonce_key
+      freeze
+    end
+
+    def nonce
+      @challenge.nonce
+    end
+  end
+end
