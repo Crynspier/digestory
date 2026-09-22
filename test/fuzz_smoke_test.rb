81025e7cd2d543638f702f01d4e0c396f0863138
Leon Zjakić <117075643+Crynspier@users.noreply.github.com>
2026-09-22 12:01:38 +0200

    test: expand parser malformed-input corpus

--- test/fuzz_smoke_test.rb
+++ test/fuzz_smoke_test.rb
@@ -5,7 +5,7 @@ require_relative "test_helper"
 class FuzzSmokeTest < Minitest::Test
   def test_parser_survives_deterministic_malformed_corpus
     seed = 0xD16570
-    500.times do |i|
+    2000.times do |i|
       rng = Random.new(seed + i)
       candidate = Array.new(rng.rand(0..160)) { rng.rand(0..255).chr }.join
       begin
@@ -16,3 +16,16 @@ class FuzzSmokeTest < Minitest::Test
     end
   end
 end
+
+
+  def test_parser_handles_common_qop_spacing_corpus
+    [
+      'Digest realm="r", nonce="n", algorithm=SHA-256, qop="auth,auth-int"',
+      'Digest realm="r", nonce="n", algorithm=SHA-256, qop="auth, auth-int"',
+      'Digest realm="r", nonce="n", algorithm=SHA-256, qop="auth , auth-int"',
+      'Digest realm="r", nonce="n", algorithm=SHA-256, qop=" auth, auth-int "'
+    ].each do |header|
+      challenge = Digestory::Challenge.parse(header)
+      assert_equal %w[auth auth-int], challenge.qop
+    end
+  end
